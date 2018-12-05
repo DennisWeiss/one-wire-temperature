bc03fd220bef1f2e934564a63edfd8e9d38aa3b2
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-12-05 11:32:52 +0100

    Added functionality to read data

---
+++ writer.py
@@ -0,0 +1,13 @@
+file = open('/sys/devices/w1_bus_master1/w1_master_slaves')
+w1_slaves = file.readlines()
+
+for line in w1_slaves:
+    w1_slave = line.split('\n')[0]
+    file = open('/sys/bus/w1/devices/' + str(w1_slave))
+    filecontent = file.read()
+    file.close()
+    
+    stringvalue = filecontent.split('\n')[1].split(' ')[9]
+    temperature = float(stringvalue[2:]) / 1000
+    
+    print(str(w1_slave) + ': %6.2f C' % temperature)
